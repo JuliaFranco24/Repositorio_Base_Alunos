912ea49098200bfc6d70e1e5e22bce8b3f6596ee
JuliaFranco24 <juliamarquesfa09@gmail.com>
2025-09-10 13:58:59 -0300

    Add files via upload

---
+++ Modulo 1/Codigos Basicos/atv1.py
@@ -0,0 +1,31 @@
+print("Digite um número:")
+numero = int(input())
+
+print("Digite outro número:")
+numero2= int (input())
+
+print(numero+numero2)
+
+print("Digite um número:")
+numero = int(input())
+
+print("Digite outro número:")
+numero2= int (input())
+
+print(numero-numero2)
+
+print("Digite um número:")
+numero = int(input())
+
+print("Digite outro número:")
+numero2= int (input())
+
+print(numero*numero2)
+
+print("Digite um número:")
+numero = int(input())
+
+print("Digite outro número:")
+numero2= int (input())
+
+print(numero/numero)
