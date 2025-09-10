8089484bcdc508818d35a45e98e2b480f57be11d
JuliaFranco24 <juliamarquesfa09@gmail.com>
2025-09-10 14:35:40 -0300

    Add files via upload

---
+++ Modulo 1/Loopings/Atv16pt14 (1).py
@@ -0,0 +1,7 @@
+numero=11
+
+while numero>0:
+    numero=numero-1
+    print(numero)
+print('fogo')
+ 
---
+++ Modulo 1/Loopings/Atv17pt18.py
@@ -0,0 +1,8 @@
+nota=int(input("Digite uma nota de 0-10:"))
+
+while nota<0>10:
+    print("Ok")
+
+    nota=int(input("Digite a nota novamente:"))
+print("Ok")
+
