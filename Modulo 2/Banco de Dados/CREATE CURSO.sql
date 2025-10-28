4cebdefde1938d14b4a21ca76ba8f9510c2ebfc6
JuliaFranco24 <juliamarquesfa09@gmail.com>
2025-10-28 13:41:07 -0300

    Add files via upload

---
+++ Modulo 2/Banco de Dados/CREATE CURSO.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Curso(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nome TEXT,
+carga_horaria TEXT,
+area_de_conhecimento TEXT
+
+
+)
---
+++ Modulo 2/Banco de Dados/INSERT CURSO.sql
@@ -0,0 +1,2 @@
+INSERT INTO Curso(nome,carga_horaria,area_de_conhecimento) VALUES
+('artur','47 horas','python')
